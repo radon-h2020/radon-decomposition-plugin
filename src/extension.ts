79fb830e1e1dc6220e73765841103f8fafeaf30a
Lulai Zhu <lz3510@ic.ac.uk>
2021-06-29 12:53:36 +0100

    Support accuracy enhancement

--- src/extension.ts
+++ src/extension.ts
@@ -11,10 +11,7 @@ import * as FormData from 'form-data';
 import * as http from 'http';
 import * as Stream from 'stream';
 
-const DECOMPOSITION_FEATURE_ID = 0;
-const OPTIMIZATION_FEATURE_ID = 1;
-
-const outputChannel = vscode.window.createOutputChannel("RADON Decomposition");
+const outputChannel = vscode.window.createOutputChannel('RADON Decomposition');
 const underProcessing: string[] = [];
 
 /**
@@ -25,15 +22,21 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Register the 'decompose' command
 	const decompose = vscode.commands.registerCommand('radon-dec-plugin.decompose', (uri: vscode.Uri) => {
-		runProcedure(DECOMPOSITION_FEATURE_ID, uri);
+		runDecomposition(uri);
 	});
 	context.subscriptions.push(decompose);
 
 	// Register the 'optimize' command
 	const optimize = vscode.commands.registerCommand('radon-dec-plugin.optimize', (uri: vscode.Uri) => {
-		runProcedure(OPTIMIZATION_FEATURE_ID, uri);
+		runOptimization(uri);
 	});
 	context.subscriptions.push(optimize);
+
+	// Register the 'enhance' command
+	const enhance = vscode.commands.registerCommand('radon-dec-plugin.enhance', (uri: vscode.Uri) => {
+		runEnhancement(uri);
+	});
+	context.subscriptions.push(enhance);
 }
 
 /**
@@ -42,103 +45,230 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() { }
 
 /**
- * Run a specific procedure
+ * Run architecture decomposition
  */
-async function runProcedure(featureId: number, uri: vscode.Uri) {
+async function runDecomposition(uri: vscode.Uri) {
 	outputChannel.show();
+
+	// Check if the model is already under processing
 	const modelPath = uri.path;
 	if (underProcessing.includes(modelPath)) {
 		outputChannel.appendLine('The model is already under processing. Please wait...');
 		return;
 	}
-	underProcessing.push(modelPath);
 
-	const modelName = path.basename(modelPath);
+	// Gather necessary information
 	const serverConfig = getServerConfig();
-	const tempName = generateTempName(modelName);
-	switch (featureId) {
-		case DECOMPOSITION_FEATURE_ID:
-			outputChannel.appendLine('Start architecture decomposition of ' + modelName);
-			break;
-		case OPTIMIZATION_FEATURE_ID:
-			outputChannel.appendLine('Start deployment optimization of ' + modelName);
-			break;
-		default:
-			throw new Error("Unsupported feature Id: " + featureId);
-	}
+	const modelFileName = path.basename(modelPath);
+	const modelTempName = generateTempName(modelFileName);
 
+	// Start execution of the procedure
+	outputChannel.appendLine('Start architecture decomposition of ' + modelFileName);
+	underProcessing.push(modelPath);
 	try {
-		let response: any;
-		let output: any;
+		let response = null;
+		let output = null;
 
-		// Upload the original model to the server
+		// Upload the model to the server
 		try {
-			response = await uploadFile(serverConfig, modelPath, tempName);
-			outputChannel.appendLine('Successfully uploaded the original model to the server');
+			response = await uploadFile(serverConfig, modelPath, modelTempName);
+			outputChannel.appendLine('Successfully uploaded the model to the server');
 		} catch (error) {
-			outputChannel.appendLine('Failed to upload the original model to the server');
+			outputChannel.appendLine('Failed to upload the model to the server');
 			throw error;
 		}
 
-		switch (featureId) {
-			case DECOMPOSITION_FEATURE_ID:
-				// Decompose the architecture of the model
-				try {
-					response = await decomposeModel(serverConfig, tempName);
-					output = JSON.parse(response.data.toString());
-					outputChannel.appendLine('Successfully decomposed the architecture of the model');
-				} catch (error) {
-					outputChannel.appendLine('Failed to decompose the architecture of the model');
-					throw error;
-				}
-				break;
-			case OPTIMIZATION_FEATURE_ID:
-				// Optimize the deployment of the model
-				try {
-					response = await optimizeModel(serverConfig, tempName);
-					output = JSON.parse(response.data.toString());
-					outputChannel.appendLine('Successfully optimized the deployment of the model');
-				} catch (error) {
-					outputChannel.appendLine('Failed to optimize the deployment of the model');
-					throw error;
-				}
-				break;
-			default:
-				throw new Error("Unsupported feature Id: " + featureId);
+		// Decompose the architecture of the model
+		try {
+			response = await decomposeModel(serverConfig, modelTempName);
+			output = JSON.parse(response.data.toString());
+			outputChannel.appendLine('Successfully decomposed the architecture of the model');
+		} catch (error) {
+			outputChannel.appendLine('Failed to decompose the architecture of the model');
+			throw error;
 		}
 
-		// Download the resultant model from the server
+		// Download the model from the server
 		try {
-			response = await downloadFile(serverConfig, tempName);
+			response = await downloadFile(serverConfig, modelTempName);
 			backUpFile(modelPath);
 			writeFile(modelPath, response.data);
-			outputChannel.appendLine('Successfully downloaded the resultant model from the server');
+			outputChannel.appendLine('Successfully downloaded the model from the server');
 		} catch (error) {
-			outputChannel.appendLine('Failed to download the resultant model from the server');
+			outputChannel.appendLine('Failed to download the model from the server');
 			throw error;
 		}
 
-		switch (featureId) {
-			case DECOMPOSITION_FEATURE_ID:
-				// Print the output upon completion of decomposition
-				outputChannel.appendLine('Architecture decomposition of ' + modelName + ' complete');
-				outputChannel.appendLine(JSON.stringify(output, null, 2));
-				break;
-			case OPTIMIZATION_FEATURE_ID:
-				// Print the output upon completion of optimization
-				outputChannel.appendLine('Deployment optimization of ' + modelName + ' complete');
-				outputChannel.appendLine(JSON.stringify(output, null, 2));
-				outputChannel.appendLine('Total operating cost per year: ' + output.total_cost * 24 * 356);
-				break;
-			default:
-				throw new Error("Unsupported feature Id: " + featureId);
-		}
+		// Print the output upon completion
+		outputChannel.appendLine('Architecture decomposition of ' + modelFileName + ' complete');
+		outputChannel.appendLine(JSON.stringify(output, null, 2));
 	} catch (error) {
-		// Print the error upon failure of a request
+		// Print the error if a request fails
 		outputChannel.appendLine(JSON.stringify(error, null, 2));
 	} finally {
-		// Delete the residual model from the server
-		try { await deleteFile(serverConfig, tempName); } catch { }
+		// Delete the model from the server
+		try { await deleteFile(serverConfig, modelTempName); } catch { }
+		underProcessing.splice(underProcessing.indexOf(modelPath));
+	}
+}
+
+/**
+ * Run deployment optimization
+ */
+async function runOptimization(uri: vscode.Uri) {
+	outputChannel.show();
+
+	// Check if the model is already under processing
+	const modelPath = uri.path;
+	if (underProcessing.includes(modelPath)) {
+		outputChannel.appendLine('The model is already under processing. Please wait...');
+		return;
+	}
+
+	// Gather necessary information
+	const serverConfig = getServerConfig();
+	const modelFileName = path.basename(modelPath);
+	const modelTempName = generateTempName(modelFileName);
+
+	// Start execution of the procedure
+	outputChannel.appendLine('Start deployment optimization of ' + modelFileName);
+	underProcessing.push(modelPath);
+	try {
+		let response = null;
+		let output = null;
+
+		// Upload the model to the server
+		try {
+			response = await uploadFile(serverConfig, modelPath, modelTempName);
+			outputChannel.appendLine('Successfully uploaded the model to the server');
+		} catch (error) {
+			outputChannel.appendLine('Failed to upload the model to the server');
+			throw error;
+		}
+
+		// Optimize the deployment of the model
+		try {
+			response = await optimizeModel(serverConfig, modelTempName);
+			output = JSON.parse(response.data.toString());
+			outputChannel.appendLine('Successfully optimized the deployment of the model');
+		} catch (error) {
+			outputChannel.appendLine('Failed to optimize the deployment of the model');
+			throw error;
+		}
+
+		// Download the model from the server
+		try {
+			response = await downloadFile(serverConfig, modelTempName);
+			backUpFile(modelPath);
+			writeFile(modelPath, response.data);
+			outputChannel.appendLine('Successfully downloaded the model from the server');
+		} catch (error) {
+			outputChannel.appendLine('Failed to download the model from the server');
+			throw error;
+		}
+
+		// Print the output upon completion
+		outputChannel.appendLine('Deployment optimization of ' + modelFileName + ' complete');
+		outputChannel.appendLine(JSON.stringify(output, null, 2));
+		outputChannel.appendLine('Total operating cost per year: ' + output.total_cost * 24 * 356);
+	} catch (error) {
+		// Print the error if a request fails
+		outputChannel.appendLine(JSON.stringify(error, null, 2));
+	} finally {
+		// Delete the model from the server
+		try { await deleteFile(serverConfig, modelTempName); } catch { }
+		underProcessing.splice(underProcessing.indexOf(modelPath));
+	}
+}
+
+/**
+ * Run accuracy enhancement
+ */
+async function runEnhancement(uri: vscode.Uri) {
+	outputChannel.show();
+
+	// Check if the model is already under processing
+	const modelPath = uri.path;
+	if (underProcessing.includes(modelPath)) {
+		outputChannel.appendLine('The model is already under processing. Please wait...');
+		return;
+	}
+
+	// Gather necessary information
+	const serverConfig = getServerConfig();
+	const modelFileName = path.basename(modelPath);
+	const modelTempName = generateTempName(modelFileName);
+	let dataFileName = '';
+	const directoryPath = path.dirname(modelPath);
+	const directoryEntries = fs.readdirSync(directoryPath, { withFileTypes: true });
+	for (const entry of directoryEntries) {
+		if (entry.isFile() && entry.name.endsWith('.log')) {
+			dataFileName = entry.name;
+		}
+	}
+	if (!dataFileName) {
+		outputChannel.appendLine('Cannot find the data file (.log)');
+		return;
+	}
+	const dataPath = path.join(directoryPath, dataFileName);
+	const dataTempName = generateTempName(dataFileName);
+
+	// Start execution of the procedure
+	outputChannel.appendLine('Start accuracy enhancement of ' + modelFileName);
+	underProcessing.push(modelPath);
+	try {
+		let response = null;
+		let output = null;
+
+		// Upload the model to the server
+		try {
+			response = await uploadFile(serverConfig, modelPath, modelTempName);
+			outputChannel.appendLine('Successfully uploaded the model to the server');
+		} catch (error) {
+			outputChannel.appendLine('Failed to upload the model to the server');
+			throw error;
+		}
+
+		// Upload the data to the server
+		try {
+			response = await uploadFile(serverConfig, dataPath, dataTempName);
+			outputChannel.appendLine('Successfully uploaded the data to the server');
+		} catch (error) {
+			outputChannel.appendLine('Failed to upload the data to the server');
+			throw error;
+		}
+
+		// Enhance the accuracy of the model
+		try {
+			response = await enhanceModel(serverConfig, modelTempName, dataTempName);
+			output = JSON.parse(response.data.toString());
+			outputChannel.appendLine('Successfully enhanced the accuracy of the model');
+		} catch (error) {
+			outputChannel.appendLine('Failed to enhance the accuracy of the model');
+			throw error;
+		}
+
+		// Download the model from the server
+		try {
+			response = await downloadFile(serverConfig, modelTempName);
+			backUpFile(modelPath);
+			writeFile(modelPath, response.data);
+			outputChannel.appendLine('Successfully downloaded the model from the server');
+		} catch (error) {
+			outputChannel.appendLine('Failed to download the model from the server');
+			throw error;
+		}
+
+		// Print the output upon completion
+		outputChannel.appendLine('Accuracy enhancement of ' + modelFileName + ' complete');
+		outputChannel.appendLine(JSON.stringify(output, null, 2));
+	} catch (error) {
+		// Print the error if a request fails
+		outputChannel.appendLine(JSON.stringify(error, null, 2));
+	} finally {
+		// Delete the model and data from the server
+		try { await deleteFile(serverConfig, modelTempName); } catch { }
+		try { await deleteFile(serverConfig, dataTempName); } catch { }
 		underProcessing.splice(underProcessing.indexOf(modelPath));
 	}
 }
@@ -208,7 +338,7 @@ function uploadFile(serverConfig: any, uploadPath: string, fileName: string): Pr
 	return sendRequest({
 		host: serverConfig.domainName,
 		port: serverConfig.publicPort,
-		path: '/files/' + fileName,
+		path: '/file/' + fileName,
 		method: 'POST',
 		headers: formData.getHeaders()
 	}, formData);
@@ -221,7 +351,7 @@ function downloadFile(serverConfig: any, fileName: string): Promise<any> {
 	return sendRequest({
 		host: serverConfig.domainName,
 		port: serverConfig.publicPort,
-		path: '/files/' + fileName,
+		path: '/file/' + fileName,
 		method: 'GET',
 		headers: {
 			'Content-Length': 0
@@ -236,7 +366,7 @@ function deleteFile(serverConfig: any, fileName: string): Promise<any> {
 	return sendRequest({
 		host: serverConfig.domainName,
 		port: serverConfig.publicPort,
-		path: '/files/' + fileName,
+		path: '/file/' + fileName,
 		method: 'DELETE',
 		headers: {
 			'Content-Length': 0
@@ -247,11 +377,11 @@ function deleteFile(serverConfig: any, fileName: string): Promise<any> {
 /**
  * Decompose the architecture of a RADON model
  */
-function decomposeModel(serverConfig: any, fileName: string): Promise<any> {
+function decomposeModel(serverConfig: any, modelFileName: string): Promise<any> {
 	return sendRequest({
 		host: serverConfig.domainName,
 		port: serverConfig.publicPort,
-		path: '/dec-tool/decompose?filename=' + fileName,
+		path: '/dec-tool/decompose?model_filename=' + modelFileName,
 		method: 'PATCH',
 		headers: {
 			'Content-Length': 0
@@ -262,11 +392,26 @@ function decomposeModel(serverConfig: any, fileName: string): Promise<any> {
 /**
  * Optimize the deployment of a RADON model
  */
-function optimizeModel(serverConfig: any, fileName: string): Promise<any> {
+function optimizeModel(serverConfig: any, modelFileName: string): Promise<any> {
 	return sendRequest({
 		host: serverConfig.domainName,
 		port: serverConfig.publicPort,
-		path: '/dec-tool/optimize?filename=' + fileName,
+		path: '/dec-tool/optimize?model_filename=' + modelFileName,
+		method: 'PATCH',
+		headers: {
+			'Content-Length': 0
+		}
+	});
+}
+
+/**
+ * Enhance the accuracy of a RADON model
+ */
+function enhanceModel(serverConfig: any, modelFileName: string, dataFileName: string): Promise<any> {
+	return sendRequest({
+		host: serverConfig.domainName,
+		port: serverConfig.publicPort,
+		path: '/dec-tool/enhance?model_filename=' + modelFileName + '&data_filename=' + dataFileName,
 		method: 'PATCH',
 		headers: {
 			'Content-Length': 0
